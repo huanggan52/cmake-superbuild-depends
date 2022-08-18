f46fb32eb629e3982075d5af98ae2941d59f920b
huanggan1996 <1367876650@qq.com>
2022-08-18 14:35:24 +0800

    test

--- depends/lightvis.cmake
+++ depends/lightvis.cmake
@@ -2,7 +2,7 @@ if(NOT TARGET depends::lightvis)
   FetchContent_Declare(
     depends-lightvis
     GIT_REPOSITORY https://github.com/huanggan52/lightvis.git
-    GIT_TAG        xrslam
+    GIT_TAG        test
   )
   FetchContent_GetProperties(depends-lightvis)
   if(NOT depends-lightvis_POPULATED)
@@ -12,7 +12,7 @@ if(NOT TARGET depends::lightvis)
   endif()
   add_subdirectory(${depends-lightvis_SOURCE_DIR} ${depends-lightvis_BINARY_DIR})
   add_library(depends::lightvis INTERFACE IMPORTED GLOBAL)
-  target_link_libraries(depends::lightvis INTERFACE lightvis ${OpenCV_LIBS})
+  target_link_libraries(depends::lightvis INTERFACE lightvis)
   set(depends-lightvis-source-dir ${depends-lightvis_SOURCE_DIR} CACHE INTERNAL "" FORCE)
   set(depends-lightvis-binary-dir ${depends-lightvis_BINARY_DIR} CACHE INTERNAL "" FORCE)
   mark_as_advanced(depends-lightvis-source-dir)

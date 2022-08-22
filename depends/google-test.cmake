2bbf13cbce35711ec1d5361f5fc072c7b7968ab2
huanggan1996 <1367876650@qq.com>
2022-08-22 18:03:04 +0800

    1

--- depends/google-test.cmake
+++ depends/google-test.cmake
@@ -1,7 +1,4 @@
 if(NOT TARGET depends::google-test)
-  if(NOT TARGET options::modern-cpp)
-    message(FATAL_ERROR "depends::yaml-cpp expects options::modern-cpp")
-  endif()
   FetchContent_Declare(
       depends-google-test
       GIT_REPOSITORY https://github.com/google/googletest.git
@@ -13,16 +10,12 @@ if(NOT TARGET depends::google-test)
       FetchContent_Populate(depends-google-test)
       message(STATUS "Fetching google-test sources - done")
   endif()
+  set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+  set(INSTALL_GTEST     OFF)
+  
   add_subdirectory(${depends-google-test_SOURCE_DIR} ${depends-google-test_BINARY_DIR})
   add_library(depends::google-test INTERFACE IMPORTED GLOBAL)
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    target_compile_definitions(googletest
-      PUBLIC
-        _NOEXCEPT=noexcept
-    )
-  endif()
-  target_include_directories(depends::google-test INTERFACE ${depends-google-test_SOURCE_DIR}/include)
-  target_link_libraries(depends::google-test INTERFACE googletest options::modern-cpp)
+  target_link_libraries(depends::google-test INTERFACE googletest)
   set(depends-google-test-source-dir ${depends-google-test_SOURCE_DIR} CACHE INTERNAL "" FORCE)
   set(depends-google-test-binary-dir ${depends-google-test_BINARY_DIR} CACHE INTERNAL "" FORCE)
   mark_as_advanced(depends-google-test-source-dir)
